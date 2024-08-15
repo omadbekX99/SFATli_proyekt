769b014daee424944aea00f6579812681ae9478c
omadbekX99 <omadbekxaydarov978@.com>
2024-08-15 20:17:16 +0500

    initial project

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogapp'
+    name = 'myapp'
